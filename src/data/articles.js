1740323657a706cdbe490bd47468a10c19d7baf8
unknown <liarahmaasnaini1010@gmail.com>
2025-07-18 00:17:17 +0700

    Initial commit: Add project files

---
+++ src/data/articles.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function article_1() {
+	return {
+		title : "All Portfolio"
+	};
+}
+
+function article_2() {
+	return {
+	};
+}
+
+const myArticles = [article_1, article_2];
+
+export default myArticles;
